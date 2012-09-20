232d63814671cc19a30799b8e983b485bc75cf97
Clemens Stolle <clemens@Clemenss-MacBook-Pro.local>
2012-09-20 16:41:18 +0200

    refactor. thanks @maxogden for the advice
    - when done optionally calls callback with entire document

--- Readme.md
+++ Readme.md
@@ -10,14 +10,29 @@ var csv = require('csv-stream'),
     fs = require('fs')
 
 var fstream = fs.createReadStream('/path/to/file'),
-    parser = csv(/* options */)
-
-fstream.pipe(parser)
+    parser = csv(options /* optional */, callback /* optional */)
 
 // emits the current line as an array and the line number
 parser.on('data', function (data, lineNo) {
   // do stuff with data
 })
+
+// AND/OR
+
+parser = csv(/*options,*/ function (err, doc) {
+  if (err) throw err
+
+  // now you have the complete parsed document
+  doc.forEach(function (line) {
+    /* 
+     * doc is an array of lines from the csv input
+     * each line is itself an array of fields
+     */
+  })
+})
+
+// now pump some data into it
+fstream.pipe(parser)
 
 ```
 
--- csv-stream.js
+++ csv-stream.js
@@ -1,82 +1,99 @@
-var Stream = require('stream')
+var stream = require('stream'),
+    util = require('util')
 
-module.exports = function csvStream(opts) {
+function CSVStream(opts, cb) {
+  stream.Stream.call(this)
+  this.writable = true
+  this.readable = true
+  if (cb) this.cb = cb
+  if (typeof opts === 'function') this.cb = opts
+  
   opts = opts || {}
-  var delimiter = opts.delimiter || ',',
-      newline = opts.newline || '\n',
-      quote = opts.quote || '\"',
-      empty = -1
-
-  if (opts.hasOwnProperty('empty')) {
-    empty = opts.empty
-  }
-
-  var s = new Stream()
-  s.writable = true
-  s.readable = true
-
-  s.write = function (chunk) {
-    if (Buffer.isBuffer(chunk)) chunk = chunk.toString()
-
-    try {
-      parse(chunk)
-    } catch (err) {
-      this.emit('error', err)
-    }
-    
-    return true
-  }
-
-  s.end = function (buf) {
-    if (arguments.length) s.write(buf)
-
-    s.writable = false
-    s.emit('end')
-  }
-
-  s.destroy = function () {
-    s.writable = false
-    s.emit('close')
-  }
+  this.delimiter = opts.delimiter || ','
+  this.newline = opts.newline || '\n'
+  this.quote = opts.quote || '\"'
+  this.empty = -1
 
   // state
-  var line = [], 
-      field = '',
-      isQuoted = false,
-      lineNo = 0
+  this.body = []
+  this.isQuoted = false
+  this.line = []
+  this.field = ''
+  this.lineNo = 0
 
-  function parse (data) {
-    var c
-    for (var i = 0; i < data.length; i++) {
-      c = data.charAt(i)
-      
-      if (c === quote && data.charAt(i + 1) !== quote) {
-        isQuoted = isQuoted ? false : true
-        continue
-      }
-
-      if (!isQuoted && c === delimiter) {
-        if (field === '' && empty !== -1) field = empty
-        line.push(field)
-        field = ''
-        continue
-      }
-      
-      if (!isQuoted && c === '\n') {
-        line.push(field)
-        s.emit('data', line, lineNo)
-        lineNo += 1
-        field = ''
-        line = []
-        isQuoted = false
-        continue
-      }
-
-      // append current char to field string
-      field += c
-    }
-  }
-  
-  return s
+  this.on('error', function(err) {
+    if (this.cb) this.cb(err)
+  })
 }
 
+util.inherits(CSVStream, stream.Stream)
+
+CSVStream.prototype.write = function (chunk) {
+  if (Buffer.isBuffer(chunk)) chunk = chunk.toString()
+
+  try {
+    this.parse(chunk)
+  } catch (err) {
+    this.emit('error', err)
+  }
+  return true
+}
+
+CSVStream.prototype.parse = function (data) {
+  var c
+  for (var i = 0; i < data.length; i++) {
+    c = data.charAt(i)
+    
+    if (c === this.quote && data.charAt(i + 1) !== this.quote) {
+      this.isQuoted = this.isQuoted ? false : true
+      continue
+    }
+
+    if (!this.isQuoted && c === this.delimiter) {
+      if (this.field === '' && this.empty !== -1) this.field = this.empty
+      this.line.push(this.field)
+      this.field = ''
+      continue
+    }
+    
+    if (!this.isQuoted && c === '\n') {
+      this.line.push(this.field)
+
+      this.emit('data', this.line, this.lineNo)
+
+
+      this.body.push(this.line)
+      this.lineNo += 1
+
+      // reset state
+      this.field = ''
+      this.line = []
+      this.isQuoted = false
+      continue
+    }
+
+    // append current char to field string
+    this.field += c
+  }
+}
+
+CSVStream.prototype.end = function (buf) {
+  if (arguments.length) this.write(buf)
+
+  this.writable = false
+  this.readable = false
+  this.emit('end')
+  if (this.cb) this.cb(null, this.body)
+  this.emit('close')
+}
+
+CSVStream.prototype.destroy = function () {
+  this.writable = false
+  this.emit('close')
+}
+
+module.exports = function (opts, cb) {
+  return new CSVStream(opts, cb)
+}
+
+module.exports.CSVStream = CSVStream
