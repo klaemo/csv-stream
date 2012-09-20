1e7b347d01e33b27807558f3f2c2380679b17d24
Clemens Stolle <clemens@Clemenss-MacBook-Pro.local>
2012-09-20 16:42:25 +0200

    "test" callback

--- test/csv-stream.js
+++ test/csv-stream.js
@@ -1,11 +1,17 @@
 var assert = require('assert'),
     csv = require('../csv-stream'),
     fs = require('fs')
+    
+//var spec = require('stream-spec')
 
 var fstream = fs.createReadStream(__dirname + '/fixtures/quote.csv'),
-    parser = csv(),
+    parser = csv(function (err, doc) {
+      if (err) return console.error(err)
+      console.log(doc)
+    }),
     start = Date.now()
 
+//spec(parser).through().validateOnExit()
 fstream.pipe(parser)
 
 parser.on('data', function (data, line) {
