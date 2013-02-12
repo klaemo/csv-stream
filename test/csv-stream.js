55f04a0f60728e37b1f422841e3ed7cd77ffa474
klaemo <clemens@Clemenss-MacBook-Pro.local>
2013-02-12 23:47:23 +0100

    use mocha for better tests
    TODO:
    - add more tests, e.g. for the various options

--- test/csv-stream.js
+++ test/csv-stream.js
@@ -1,42 +1,104 @@
+/*jshint undef:false */
 var assert = require('assert'),
     csv = require('../csv-stream'),
-    fs = require('fs'),
-    stdout = process.stdout
-    
-//var spec = require('stream-spec')
+    fs = require('fs')
 
 var fstream = fs.createReadStream(__dirname + '/fixtures/quote.csv'),
-    parser = csv(callback),
-    start = Date.now()
+    start = Date.now(),
+    tests = {}
 
-function callback(err, doc) {
-  if (err) throw err
-  stdout.write('Should call back with entire parsed doc ')
-  try {
-    assert.equal(doc.length, 12)
-    stdout.write('\u2713\n')
-  } catch (err) {
-    console.error(err)
-  }
-}
-//spec(parser).through().validateOnExit()
-fstream.pipe(parser)
+describe('without callback', function() {
+  it('should emit data once per line', function (done) {
+    var count = 0,
+        parser = csv(),
+        fstream = fs.createReadStream(__dirname + '/fixtures/quote.csv')
 
-var count = 0
-parser.on('data', function (data, line) {
-  count += 1
+    parser.on('data', function (data, lineNo) {
+      assert.equal(lineNo, count)
+      assert(Array.isArray(data))
+      assert.equal(data.length, 2)
+
+      count += 1
+    })
+
+    parser.on('end', function () {
+      assert.equal(count, 12)
+
+      done()
+    })
+
+
+    fstream.pipe(parser)
+  })
 })
 
-parser.on('end', function () {
-  stdout.write('Should emit data once per line ')
-  try {
-    assert.equal(count, 12)
-    stdout.write('\u2713\n')
-  } catch (err) {
-    console.error(err)
-  }
-  
-  console.log('Time: ' + (Date.now() - start) + 'ms')
+describe('with callback', function() {
+  it('should callback with entire parsed document', function (done) {
+    var parser = csv(cb),
+        fstream = fs.createReadStream(__dirname + '/fixtures/quote.csv')
+
+    function cb (err, doc) {
+      if (err) throw err
+
+      assert(Array.isArray(doc))
+      assert.equal(doc.length, 12)
+
+      done()
+    }
+
+    fstream.pipe(parser)
+  })
+})
+
+describe('quoted', function() {
+  it('should handle quoted sequences', function (done) {
+    var parser = csv(cb),
+        fstream = fs.createReadStream(__dirname + '/fixtures/quote.csv')
+
+    function cb (err, doc) {
+      if (err) throw err
+
+      assert.equal(doc[1][1], 'Etiketten, "Borthener Obst" - A4 (Neutral)')
+
+      done()
+    }
+
+    fstream.pipe(parser)
+  })
+})
+
+describe('encoding', function() {
+  it('should convert encoding options is set', function (done) {
+    var parser = csv({ encoding: 'latin1' }, cb),
+        fstream = fs.createReadStream(__dirname + '/fixtures/quote.csv')
+
+    function cb (err, doc) {
+      if (err) throw err
+
+      assert.equal(doc[5][1], 'Gröger')
+      assert.equal(doc[7][1], '1 - 4/- Blätter(R505)')
+
+      done()
+    }
+
+    fstream.pipe(parser)
+  })
+
+  it('should not convert encoding if option is not set', function (done) {
+    var parser = csv(cb),
+        fstream = fs.createReadStream(__dirname + '/fixtures/quote.csv')
+
+    function cb (err, doc) {
+      if (err) throw err
+
+      assert.notEqual(doc[5][1], 'Gröger')
+      assert.notEqual(doc[7][1], '1 - 4/- Blätter(R505)')
+
+      done()
+    }
+
+    fstream.pipe(parser)
+  })
 })
 
 
