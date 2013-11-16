562aea13d31a56ed340dfab08f16f667ba347fae
Clemens Stolle <clemens@Clemenss-MacBook-Pro.local>
2013-11-16 14:32:09 +0100

    try to appease the CI gods

--- test/csv-spectrum.js
+++ test/csv-spectrum.js
@@ -3,31 +3,30 @@ var assert = require('assert'),
     fs = require('fs'),
     path = require('path'),
     spectrum = require('csv-spectrum'),
-    csvPath = path.join(process.cwd(), 'node_modules', 'csv-spectrum', 'csvs')
+    csvPath = path.join(process.cwd(), 'node_modules', 'csv-spectrum', 'csvs'),
+    async = require('async')
 
 spectrum(function (err, data) {
   if (err) throw err
   
   describe('csv-spectrum tests', function() {
-    data.forEach(function (spec) {
+    async.each(data, function (spec, cb) {
       it(spec.name, function (done) {
         var opts = { columns: true }
         // set encoding
         if (spec.name == 'latin1') opts.inputEncoding = 'latin1'
 
         if (/._crlf/.test(spec.name)) opts.newline = '\r\n'
-        var parser = csv(opts, cb)
         var s = fs.createReadStream(path.join(csvPath, spec.name + '.csv'))
 
-        function cb (err, doc) {
+        var parser = csv(opts, function parsed (err, doc) {
           if (err) return done(err)
-
-          try {
-            assert.deepEqual(doc, JSON.parse(spec.json))
-          } catch(e) { return done(e) }
+          console.log(spec.name)
+          assert.deepEqual(doc, JSON.parse(spec.json))
           done()
-        }
-
+          cb()
+        })
+        
         s.pipe(parser)
       })
     })
