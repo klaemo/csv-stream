b2d331582d3d4bc317aac1f04c22107d4bedc684
Clemens Stolle <clemens@Clemenss-MacBook-Pro.local>
2013-11-16 14:52:55 +0100

    why you no pass on node 0.8 on travis

--- test/csv-spectrum.js
+++ test/csv-spectrum.js
@@ -2,34 +2,33 @@ var assert = require('assert'),
     csv = require('../csv-streamify'),
     fs = require('fs'),
     path = require('path'),
-    spectrum = require('csv-spectrum'),
-    csvPath = path.join(process.cwd(), 'node_modules', 'csv-spectrum', 'csvs'),
+    fs = require('fs'),
+    base = path.join(process.cwd(), 'node_modules', 'csv-spectrum'),
+    csvPath = path.join(base, 'csvs'),
     async = require('async')
 
-spectrum(function (err, data) {
-  if (err) throw err
-  
-  describe('csv-spectrum tests', function() {
-    async.each(data, function (spec, cb) {
-      it(spec.name, function (done) {
-        var opts = { columns: true }
-        // set encoding
-        if (spec.name == 'latin1') opts.inputEncoding = 'latin1'
+describe('spectrum', function() {
+  var specs = fs.readdirSync(csvPath)
+    .map(function (spec) { return spec.replace('.csv', '') })
+    
+  specs.forEach(function (spec) {
+    it(spec, function (done) {
+      var opts = { columns: true }
+      // set encoding
+      if (spec == 'latin1') opts.inputEncoding = 'latin1'
 
-        if (/._crlf/.test(spec.name)) opts.newline = '\r\n'
-        var s = fs.createReadStream(path.join(csvPath, spec.name + '.csv'))
+      if (/._crlf/.test(spec)) opts.newline = '\r\n'
+      var s = fs.createReadStream(path.join(csvPath, spec + '.csv'))
 
-        var parser = csv(opts, function parsed (err, doc) {
-          if (err) return done(err)
-          console.log(spec.name)
-          assert.deepEqual(doc, JSON.parse(spec.json))
-          done()
-          cb()
-        })
-        
-        s.pipe(parser)
+      var parser = csv(opts, function parsed (err, doc) {
+        if (err) return done(err)
+        assert.deepEqual(doc, require('csv-spectrum/json/' + spec))
+        done()
       })
-    })
 
+      s.pipe(parser)
+    })
   })
+
 })
+
