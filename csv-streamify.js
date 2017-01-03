aa9bd2cace4a5f352c8b8fd6cec8722151282102
Glen Selle <glenselle@me.com>
2017-01-03 08:57:37 -0600

    Added a columnTransform func to transform column names

--- csv-streamify.js
+++ csv-streamify.js
@@ -16,7 +16,8 @@ module.exports = function (opts, cb) {
   opts.empty = opts.hasOwnProperty('empty') ? opts.empty : ''
   opts.objectMode = opts.objectMode || false
   opts.hasColumns = opts.columns || false
-
+  opts.columnTransform = opts.columnTransform || function(l) { return l }
+  
   // state
   var state = {
     body: [],
@@ -41,7 +42,7 @@ function createParser (opts, state) {
 
     if (opts.hasColumns) {
       if (state.lineNo === 0) {
-        state._columns = state._line
+        state._columns = state._line.map(opts.columnTransform)
         state.lineNo += 1
         reset()
         return
